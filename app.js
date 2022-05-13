bc44eb17cb8269d367ccd33a7c90bab4a8420e03
Timothy <ayomidetimothy28@gmail.com>
2022-05-13 19:09:43 +0100

    added click function to the buttons

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
-const resultDispay = document.getElementById('result')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDispay = document.getElementById('result') 
+const possibleChoices = document.querySelectorAll('buttons')
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+})) 
